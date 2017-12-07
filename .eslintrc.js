a0a528484a33c0ae451f06d1ed0489628a7c8a8a
michaelvincerra <michael.vincerra@gmail.com>
2017-12-06 20:41:23 -0800

    Initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "["standard", "standard-jsx"]		
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
---
+++ src/ChartVis.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+// import Buttons from './Buttons'
+// import YearRange from './YearRange'
+
+
+const SERVER_ROOT = 'http://api.worldbank.org/'
+const USER_PATH1 = `${SERVER_ROOT}countries/it/indicators//NY.GDP.MKTP.CD?format=json&per_page=500&date=1995:2015`
+
+
+export default class ChartVis extends Component {
+    // 30.11.2017. Restart here. 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            data: []
+        }
+    }
+
+    componentWillMount () {
+        fetch(`${USER_PATH1}`)
+            .then((response) => response.json())
+            .then((data) => {
+                const filteredData = data[1].map(function(datapoint,i) { 
+                    return {
+                        'id': datapoint.country.id,
+                        'year': datapoint.date,
+                        'value': datapoint.value 
+                    }
+                })
+                this.setState({ data: filteredData }) // TODO: Check the JSON response object. 
+            })
+            .catch(error => console.log(error))
+    }
+
+    componentDidMount() {
+        // This will call the d3 to show the chart 
+    }
+
+    d3_function() {
+    }
+
+    
+    Italy_GDP_Chart(data) {    
+    }
+
+    render() {
+
+        // call js functions here 
+        console.log(this.state.data)
+        return (
+            // chart id is for SVG 
+            <section className="all_container">
+
+                <div className='container'>
+                    <div id='chart'></div>
+                    <div id='slider'></div>
+                    <p id='range-label'></p>
+                </div>
+
+            </section>
+
+        )
+    }
+}
+
+// export default ChartVis;
