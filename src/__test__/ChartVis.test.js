5f33f8fba08685464281c13d90048ef5a496a339
michaelvincerra <michael.vincerra@gmail.com>
2017-12-19 12:03:26 -0800

    Removes unused tests.

--- src/__test__/ChartVis.test.js
+++ src/__test__/ChartVis.test.js
@@ -1,9 +1,6 @@
 
-// Create unittest that verifies that a small unit of data can be passed.
-// Jest allows us to write unittests using Snapshot feature
-
 import React from 'react'
-import Enzyme, { shallow, mount } from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import ChartVis from '../ChartVis'
 import testData from './testData'
@@ -28,15 +25,7 @@ describe('ChartVis', () => {
     app = wrapper.instance()
   })
 
-  it('displays a chart', (done) => {
-    setTimeout(function () {
-      console.log(wrapper.find('#barChart'))
-      expect(wrapper.find('#barChart').exists()).toBe(true)
-      done()
-    }, 1000)
-  })
-
-  it('when fetchData is called, state is updated', () => {
+  it('Updates the state data when fetchData is called', () => {
     let stateData = wrapper.state()
     let expected = [
 
@@ -66,21 +55,3 @@ describe('ChartVis', () => {
     expect(expected).toEqual(stateData.data[0].values)
   })
 })
-// const filteredData = [
-//   parseData(data) {
-//     const filteredData = [
-//       {
-//         name: data[1][0].country.id,
-//         values: []
-//       }
-//     ]
-//   const plotData = data[1].map(function (datapoint, i) {
-//       return {
-//         'x': datapoint.date,
-//         'y': datapoint.value
-//       }
-//     })
-//   filteredData[0].values = plotData.reverse()
-//   console.log(filteredData)
-//   this.setState({ data: filteredData })
-//   }
